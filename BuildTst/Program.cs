2b197ec4587b525011fe9c31962a771027646f25
simon.vezey <simon.vezey@activus.co.uk>
2017-12-08 17:01:20 +0000

    Add project files.

---
+++ BuildTst/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BuildTst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
